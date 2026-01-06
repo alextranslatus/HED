3f663760ed4db40af09b8046ffb717c8c923951e
Alex Sheridan <alexsheridan.pro@outlook.fr>
2026-01-06 12:02:01 +0100

    6th

--- script.R
+++ script.R
@@ -27,3 +27,73 @@ for(
 # https://cls.ucl.ac.uk/cls-studies/millennium-cohort-study/
 # https://ukdataservice.ac.uk/
 
+load("data/mcs_hed.RData")
+dict_mcs <- read_excel("data/mcs_catalog.xlsx")
+
+
+ggplot(mcs_hed, aes(APCHUN00_m)) +
+  geom_bar()
+
+mcs_hed <- as.data.table(mcs_hed)
+
+mcs_hed[APCHUN00_m > 0, childoutofmarriage:= APCHUN00_m]
+
+ggplot(mcs_hed, aes(childoutofmarriage)) +
+  geom_bar()
+
+str(mcs_hed$childoutofmarriage)
+
+mcs_hed$childoutofmarriage <- as.factor(mcs_hed$childoutofmarriage)
+
+str(mcs_hed$childoutofmarriage)
+
+levels(mcs_hed$childoutofmarriage) <- c("Strongly agree", "Agree", "Neither agree nor disagree", "Disagree", "Strongly disagree", "Can t say")
+
+table(mcs_hed$childoutofmarriage)
+
+ggplot(mcs_hed, aes(childoutofmarriage)) +
+  geom_bar()
+
+ggplot(mcs_hed, aes(childoutofmarriage)) +
+  geom_bar() +
+  coord_flip()
+
+mcs_hed <- mcs_hed %>%
+  drop_na(childoutofmarriage)
+
+ggplot(mcs_hed, aes(childoutofmarriage)) +
+  geom_bar() +
+  coord_flip()
+
+ggplot(mcs_hed, aes(childoutofmarriage)) +
+  geom_bar() +
+  coord_flip() +
+  theme_minimal() +
+  labs(title = "All right to have children without being married",
+       x = "",
+       y = "Counts")
+
+ggplot(mcs_hed, aes(childoutofmarriage)) +
+  geom_bar() +
+  coord_flip() +
+  theme_minimal() +
+  labs(title = "All right to have children without being married",
+       x = "",
+       y = "Counts")
+
+ggplot(mcs_hed, aes(x = 1, fill = childoutofmarriage)) +
+  geom_bar(position = "likert") +
+  coord_flip() +
+  theme_minimal() +
+  labs(title = "All right to have children without being married",
+       x = "",
+       y = "Proportion",
+       fill = "") +
+  theme(axis.ticks.y = element_blank(),
+        axis.text.y = element_blank(),
+        legend.position = "bottom") +
+  guides(fill = guide_legend(nrow = 2))
+
+
+
+
